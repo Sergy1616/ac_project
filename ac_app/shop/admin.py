e131396e72a25e7d63d3d27293eaede351c2d54b
Sergy1616 <sqtsarev@gmail.com>
2024-05-29 18:03:27 +0200

    shop/models: Category, Brand, Product, ProductImage

--- ac_app/shop/admin.py
+++ ac_app/shop/admin.py
@@ -1,3 +1,56 @@
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django import forms
 from django.contrib import admin
+from .models import Category, Brand, ProductImage, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug')
+    list_display_links = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'image')
+    list_display_links = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class ProductAdminForm(forms.ModelForm):
+    description = forms.CharField(widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    form = ProductAdminForm
+    list_display = (
+        'id', 
+        'category',
+        'name',
+        'brand',
+        'price',
+        'discount',
+        'final_price_display',
+        'in_stock'
+    )
+    search_fields = ('name', 'brand',)
+    list_display_links = ('name',)
+    list_editable = ('price', 'discount', 'in_stock',)
+    list_filter = ('category', 'in_stock', 'time_create')
+    prepopulated_fields = {'slug': ('name',)}
+    inlines = (ProductImageInline,)
+
+    def final_price_display(self, obj):
+        return obj.final_price()
+    final_price_display.short_description = 'Final Price'
--- ac_app/shop/models.py
+++ ac_app/shop/models.py
@@ -1,3 +1,68 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True, unique=True)
+    slug = models.SlugField(max_length=100, db_index=True, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'categories'
+        ordering = ['name']
+
+
+class Brand(models.Model):
+    def upload_img(self, filename):
+        return f'brands/{self.slug}/{filename}'
+
+    name = models.CharField(max_length=100, db_index=True, unique=True, verbose_name='Brand')
+    slug = models.SlugField(max_length=100, db_index=True, unique=True)
+    image = models.ImageField(upload_to=upload_img, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='products')
+    name = models.CharField(max_length=100, unique=True, db_index=True)
+    slug = models.SlugField(max_length=100, unique=True, db_index=True)
+    brand = models.ForeignKey(Brand, models.SET_NULL, blank=True, null=True, default=None)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    discount = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)],
+        verbose_name='Discount %',
+        help_text='Percentage value (0 to 100)'
+    )
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_updated = models.DateTimeField(auto_now=True)
+    in_stock = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-time_create', 'name']
+
+    def final_price(self):
+        if self.discount:
+            discount_amount = (self.price * self.discount) / 100
+            return round(self.price - discount_amount, 2)
+        return self.price
+
+
+class ProductImage(models.Model):
+    def upload_img(self, filename):
+        return f'product_images/{self.product.category.slug}/{self.product.slug}/{filename}'
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to=upload_img, blank=True)
+    title_image = models.BooleanField(default=False)
+    is_for_slider = models.BooleanField(default=False)
